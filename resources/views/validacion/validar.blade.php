2c2e115de953feffe95c62b6422e86d9ca28dc8e
Jorge Henriquez <jhenriquez@supertrans.cl>
2025-07-09 14:56:05 -0400

    Versión con ajustes menores

--- resources/views/validacion/validar.blade.php
+++ resources/views/validacion/validar.blade.php
@@ -3,6 +3,12 @@
         <h2 class="text-xl font-semibold text-gray-800">Validar Pedido #{{ $pedido->referencia }}</h2>
     </x-slot>
 
+    @if(session('status'))
+                        <div class="mb-4 px-4 py-4 bg-red-100 text-red-800 border border-red-400 rounded text-center text-lg font-semibold">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">              
